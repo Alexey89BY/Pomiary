8a30c7b3c81fcaabf6b0a6f218241f33f7f7a48f
AlexeyProkhorov3389 <36195072+Alexey89BY@users.noreply.github.com>
2023-09-18 01:41:08 +0200

    feat: show point check result when user is typing

---
+++ app/src/main/java/local/tools/pomiary/ui/main/PointTextWatcher.kt
@@ -0,0 +1,30 @@
+package local.tools.pomiary.ui.main
+
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import local.tools.pomiary.DataStorage
+import local.tools.pomiary.PointsAligner
+
+
+class PointTextWatcher(
+    private val tolerance: DataStorage.PointTolerance,
+    private val textsResult: TextView
+) : TextWatcher {
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        val pointValue = PointsAligner.roundPoint(s.toString().toFloatOrNull() ?: 0.0F)
+        val pointResult = PointsAligner.testPoint(pointValue, tolerance)
+        textsResult.setTextColor(PointsAligner.colorByResult(pointResult))
+        textsResult.text = buildString { append(" %.1f ") }.format(pointValue)
+    }
+
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable) {
+
+    }
+}
